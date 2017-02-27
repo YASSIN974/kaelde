9b4d48d1a307c2583ce07cd7ee3e77a8c15adf19
ProgrammingWizzard <programmingwizzard@gmail.com>
2017-02-27 16:15:15 +0100

    part of 3.0

--- src/main/java/com/programmingwizzard/charrizard/Main.java
+++ src/main/java/com/programmingwizzard/charrizard/Main.java
@@ -1,11 +1,5 @@
 package com.programmingwizzard.charrizard;
 
-import com.programmingwizzard.charrizard.bot.Charrizard;
-import com.programmingwizzard.charrizard.bot.Settings;
-import com.programmingwizzard.charrizard.utils.GsonUtils;
-
-import java.io.File;
-
 /*
  * @author ProgrammingWizzard
  * @date 04.02.2017
@@ -13,8 +7,6 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Settings settings = GsonUtils.readConfiguration(Settings.class, new File("config.json"));
-        Charrizard charrizard = new Charrizard(settings);
-        charrizard.start();
+
     }
 }
--- src/main/java/com/programmingwizzard/charrizard/bot/response/kiciusie/KiciusieMode.java
+++
@@ -1,9 +0,0 @@
-package com.programmingwizzard.charrizard.bot.response.kiciusie;
-
-/*
- * @author Libter
- * @date 10.02.2017
- */
-public enum KiciusieMode {
-    RANDOM, IMAGE, GIF
-}
