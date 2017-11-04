1a23fdd786164c578d84b3e07f5e360dcd185f27
gabixdev <gabixdev@gabixdev.cba.pl>
2017-11-04 18:20:56 +0100

    Fix ky!cat command.

--- src/main/java/me/gabixdev/kyoko/command/fun/CatCommand.java
+++ src/main/java/me/gabixdev/kyoko/command/fun/CatCommand.java
@@ -2,10 +2,8 @@ package me.gabixdev.kyoko.command.fun;
 
 import me.gabixdev.kyoko.Kyoko;
 import me.gabixdev.kyoko.i18n.Language;
-import me.gabixdev.kyoko.remoteapi.ResponseException;
-import me.gabixdev.kyoko.remoteapi.kiciusie.KiciusieMode;
-import me.gabixdev.kyoko.remoteapi.kiciusie.KiciusieResponse;
-import me.gabixdev.kyoko.remoteapi.kiciusie.KiciusieResponses;
+import me.gabixdev.kyoko.util.GsonUtil;
+import me.gabixdev.kyoko.util.URLUtil;
 import me.gabixdev.kyoko.util.command.Command;
 import me.gabixdev.kyoko.util.command.CommandType;
 import net.dv8tion.jda.core.EmbedBuilder;
@@ -13,7 +11,6 @@ import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.events.Event;
 
 public class CatCommand extends Command {
-    private final KiciusieResponses kiciusieResponses = new KiciusieResponses();
 
     private final String[] aliases = new String[]{"cat"};
     private Kyoko kyoko;
@@ -50,38 +47,14 @@ public class CatCommand extends Command {
     @Override
     public void handle(Message message, Event event, String[] args) throws Throwable {
         Language l = kyoko.getI18n().getLanguage(message.getGuild());
-        KiciusieMode mode;
-
-        if (args.length == 1) {
-            mode = KiciusieMode.RANDOM;
-        } else if (args.length == 2) {
-            switch (args[1].toLowerCase()) {
-                case "random":
-                    mode = KiciusieMode.RANDOM;
-                    break;
-                case "image":
-                    mode = KiciusieMode.IMAGE;
-                    break;
-                case "gif":
-                    mode = KiciusieMode.GIF;
-                    break;
-                default:
-                    printUsage(kyoko, l, message.getTextChannel());
-                    return;
-            }
-        } else {
-            printUsage(kyoko, l, message.getTextChannel());
-            return;
-        }
-
 
         try {
-            KiciusieResponse response = kiciusieResponses.call(mode);
+            String url = GsonUtil.fromStringToJsonElement(URLUtil.readUrl("http://random.cat/meow")).getAsJsonObject().get("file").getAsString();
             EmbedBuilder builder = kyoko.getAbstractEmbedBuilder().getNormalBuilder();
             builder.addField(kyoko.getI18n().get(l, "cat.title"), kyoko.getI18n().get(l, "cat.subtitle"), true);
-            builder.setImage(response.getImageUrl());
+            builder.setImage(url);
             message.getChannel().sendMessage(builder.build()).queue();
-        } catch (ResponseException ex) {
+        } catch (Exception ex) {
             EmbedBuilder builder = kyoko.getAbstractEmbedBuilder().getErrorBuilder();
             builder.addField(kyoko.getI18n().get(l, "generic.error"), kyoko.getI18n().get(l, "cat.error"), false);
             message.getChannel().sendMessage(builder.build()).queue();
