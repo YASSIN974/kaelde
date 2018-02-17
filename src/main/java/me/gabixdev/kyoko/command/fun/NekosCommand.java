04d407e33ca0c16a7709f44fd228227e864322dc
Oskarr1239 <oskar000703@wp.pl>
2018-02-17 15:36:18 +0100

    odpalanie komendy ky!list w nowym watku (robione na szybko, uznaj ten request za pokazanie bledu xd

--- src/main/java/me/gabixdev/kyoko/command/fun/NekosCommand.java
+++ src/main/java/me/gabixdev/kyoko/command/fun/NekosCommand.java
@@ -2,6 +2,7 @@ package me.gabixdev.kyoko.command.fun;
 
 import me.gabixdev.kyoko.Constants;
 import me.gabixdev.kyoko.Kyoko;
+import me.gabixdev.kyoko.Settings;
 import me.gabixdev.kyoko.i18n.Language;
 import me.gabixdev.kyoko.util.GsonUtil;
 import me.gabixdev.kyoko.util.URLUtil;
@@ -51,29 +52,34 @@ public class NekosCommand extends Command
 
     @Override
     public void handle(Message message, Event event, String[] args) throws Throwable {
-        Language l = kyoko.getI18n().getLanguage(message.getMember());
-        if(args.length == 1) {
-            String type = types[RandomUtils.nextInt(0, types.length-1)];
-            String url = GsonUtil.fromStringToJsonElement(URLUtil.readUrl(nekourl + type)).getAsJsonObject().get("url").getAsString();
+        Settings settings = kyoko.getSettings();
+        if(settings.isWipFeaturesEnabled())
+        {
+            Language l = kyoko.getI18n().getLanguage(message.getMember());
+            if(args.length == 1) {
+                String type = types[RandomUtils.nextInt(0, types.length-1)];
+                String url = GsonUtil.fromStringToJsonElement(URLUtil.readUrl(nekourl + type)).getAsJsonObject().get("url").getAsString();
+                EmbedBuilder embedBuilder = kyoko.getAbstractEmbedBuilder().getNormalBuilder();
+                embedBuilder.addField(String.format(kyoko.getI18n().get(l, "nekos.title"), type), kyoko.getI18n().get(l, "nekos.subtitle"), true);
+                embedBuilder.setImage(url);
+                message.getTextChannel().sendMessage(embedBuilder.build()).queue();
+                return;
+            }
+            if(!message.getTextChannel().isNSFW() && args[1].equalsIgnoreCase("lewd")) {
+                printNSFW(kyoko, l, message.getTextChannel());
+                return;
+            }
+            if(!Arrays.asList(types).contains(args[1].toLowerCase())) {
+                printUsage(kyoko, l, message.getTextChannel());
+                return;
+            }
+            String url = GsonUtil.fromStringToJsonElement(URLUtil.readUrl(nekourl + args[1].toLowerCase())).getAsJsonObject().get("url").getAsString();
             EmbedBuilder embedBuilder = kyoko.getAbstractEmbedBuilder().getNormalBuilder();
-            embedBuilder.addField(String.format(kyoko.getI18n().get(l, "nekos.title"), type), kyoko.getI18n().get(l, "nekos.subtitle"), true);
+            embedBuilder.addField(String.format(kyoko.getI18n().get(l, "nekos.title"), args[1].toLowerCase()), kyoko.getI18n().get(l, "nekos.subtitle"), true);
             embedBuilder.setImage(url);
             message.getTextChannel().sendMessage(embedBuilder.build()).queue();
-            return;
         }
-        if(!message.getTextChannel().isNSFW() && args[1].equalsIgnoreCase("lewd")) {
-            printNSFW(kyoko, l, message.getTextChannel());
-            return;
-        }
-        if(!Arrays.asList(types).contains(args[1].toLowerCase())) {
-            printUsage(kyoko, l, message.getTextChannel());
-            return;
-        }
-        String url = GsonUtil.fromStringToJsonElement(URLUtil.readUrl(nekourl + args[1].toLowerCase())).getAsJsonObject().get("url").getAsString();
-        EmbedBuilder embedBuilder = kyoko.getAbstractEmbedBuilder().getNormalBuilder();
-        embedBuilder.addField(String.format(kyoko.getI18n().get(l, "nekos.title"), args[1].toLowerCase()), kyoko.getI18n().get(l, "nekos.subtitle"), true);
-        embedBuilder.setImage(url);
-        message.getTextChannel().sendMessage(embedBuilder.build()).queue();
+
 
     }
 }
