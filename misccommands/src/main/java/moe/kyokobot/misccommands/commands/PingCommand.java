0228f4289c939672c4dc7256a146455691ec0361
hamster <hamsterofficial@o2.pl>
2018-05-04 15:48:45 +0200

    Add ping command

---
+++ misccommands/src/main/java/moe/kyokobot/misccommands/commands/PingCommand.java
@@ -0,0 +1,26 @@
+package moe.kyokobot.misccommands.commands;
+
+import moe.kyokobot.bot.command.Command;
+import moe.kyokobot.bot.command.CommandCategory;
+import moe.kyokobot.bot.command.CommandContext;
+import moe.kyokobot.bot.manager.CommandManager;
+import java.time.temporal.ChronoUnit;
+
+public class PingCommand extends Command {
+	private CommandManager commandManager;
+
+    public PingCommand(CommandManager commandManager) {
+    	this.commandManager = commandManager;
+    	
+    	name = "ping";
+    	category = CommandCategory.UTILITY;
+    }
+    
+    @Override
+    public void execute(CommandContext context) {
+    	context.send("🏓 | Ping...", message -> {
+            long ping = context.getEvent().getMessage().getCreationTime().until(message.getCreationTime(), ChronoUnit.MILLIS);
+            message.editMessage("🏓 | Ping: " + ping + " ms | Gateway: " + context.getMessage().getJDA().getPing() + "ms").queue();
+        });
+    }
+}
