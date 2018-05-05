94c886d51cb7d6d91f5d82cab0e32585eec48bd4
gabixdev <gabixdeveloper@gmail.com>
2018-05-05 13:42:18 +0200

    fix avatar and say commands

--- misccommands/src/main/java/moe/kyokobot/misccommands/commands/AvatarCommand.java
+++ misccommands/src/main/java/moe/kyokobot/misccommands/commands/AvatarCommand.java
@@ -4,9 +4,11 @@ import moe.kyokobot.bot.command.Command;
 import moe.kyokobot.bot.command.CommandCategory;
 import moe.kyokobot.bot.command.CommandContext;
 import moe.kyokobot.bot.manager.CommandManager;
+import moe.kyokobot.bot.util.CommonErrors;
 import moe.kyokobot.bot.util.UserUtil;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.User;
 
 public class AvatarCommand extends Command {
     private CommandManager commandManager;
@@ -19,26 +21,24 @@ public class AvatarCommand extends Command {
         description = "avatar.description";
     }
 
-
     @Override
     public void execute(CommandContext context) {
-        Member member = UserUtil.getMember(context.getGuild(), context.getConcatArgs());
-        EmbedBuilder eb = context.getNormalEmbed();
         if (context.hasArgs()) {
+            Member member = UserUtil.getMember(context.getGuild(), context.getConcatArgs());
             if (member != null) {
-                if (member.getUser().getAvatarUrl() == null) {
-                    context.send(context.error() + context.getTranslated("avatar.null"));
-                }
-                eb.addField(context.getTranslated("avatar.user") + member.getUser().getName() + "#" + member.getUser().getDiscriminator(), "", false);
-                eb.setImage(member.getUser().getAvatarUrl());
-                context.send(eb.build());
+                printAvatar(context, member.getUser());
             } else {
-                context.send(context.error() + "" + context.getTranslated("avatar.usernotfound"));
+                CommonErrors.noUserFound(context, context.getConcatArgs());
             }
         } else {
-            eb.addField(context.getTranslated("avatar.user") + context.getSender().getName() + "#" + context.getSender().getDiscriminator(), "", false);
-            eb.setImage(context.getSender().getEffectiveAvatarUrl());
-            context.send(eb.build());
+            printAvatar(context, context.getSender());
         }
+    }
+
+    private void printAvatar(CommandContext context, User user) {
+        EmbedBuilder eb = context.getNormalEmbed();
+        eb.addField(String.format(context.getTranslated("avatar.user"), user.getName() + "#" + user.getDiscriminator()), String.format("[%s](%s)", context.getTranslated("avatar.direct"), user.getEffectiveAvatarUrl()), false);
+        eb.setImage(user.getEffectiveAvatarUrl());
+        context.send(eb.build());
     }
 }
