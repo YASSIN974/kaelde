110d1204c8621daecb6df5dc0f1faa094da6da35
gabixdev <gabixdeveloper@gmail.com>
2018-05-05 13:36:11 +0200

    fix UserInfo command

--- misccommands/src/main/java/moe/kyokobot/misccommands/commands/UserInfoCommand.java
+++ misccommands/src/main/java/moe/kyokobot/misccommands/commands/UserInfoCommand.java
@@ -4,6 +4,7 @@ import moe.kyokobot.bot.command.Command;
 import moe.kyokobot.bot.command.CommandCategory;
 import moe.kyokobot.bot.command.CommandContext;
 import moe.kyokobot.bot.manager.CommandManager;
+import moe.kyokobot.bot.util.CommonErrors;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
 import moe.kyokobot.bot.util.UserUtil;
@@ -13,38 +14,33 @@ public class UserInfoCommand extends Command {
 
     public UserInfoCommand(CommandManager commandManager) {
         this.commandManager = commandManager;
-
-
         name = "userinfo";
         category = CommandCategory.UTILITY;
         description = "userinfo.description";
     }
 
-
     @Override
     public void execute(CommandContext context) {
-        Member member = UserUtil.getMember(context.getGuild(), context.getConcatArgs());
-        EmbedBuilder eb = context.getNormalEmbed();
         if (context.hasArgs()) {
+            Member member = UserUtil.getMember(context.getGuild(), context.getConcatArgs());
             if (member != null) {
-                eb.setThumbnail(member.getUser().getEffectiveAvatarUrl());
-                eb.addField(context.getTranslated("userinfo.user") + member.getUser().getName() + "#" + member.getUser().getDiscriminator(), "", false);
-                eb.addField("ID: ", member.getUser().getId() + "", false);
-                eb.addField("Status:", member.getOnlineStatus() + "", false);
-                eb.addField(context.getTranslated("userinfo.game"), member.getGame() + "", false);
-                eb.addField(context.getTranslated("userinfo.roles"), member.getRoles() + "", false);
-                context.send(eb.build());
+                sendProfile(context, member);
             } else {
-                context.send(context.error() + context.getTranslated("userinfo.notfound"));
+                CommonErrors.noUserFound(context, context.getConcatArgs());
             }
         } else {
-            eb.setThumbnail(context.getSender().getEffectiveAvatarUrl());
-            eb.addField(context.getTranslated("userinfo.user") + context.getSender().getName() + "#" + context.getSender().getDiscriminator(), "", false);
-            eb.addField("ID: ", context.getSender().getId() + "", false);
-            eb.addField("Status:", context.getMember().getOnlineStatus() + "", false);
-            eb.addField(context.getTranslated("userinfo.game"), context.getMember().getGame() + "", false);
-            eb.addField(context.getTranslated("userinfo.roles"), context.getMember().getRoles() + "", false);
-            context.send(eb.build());
+            sendProfile(context, context.getMember());
         }
+    }
+
+    private void sendProfile(CommandContext context, Member member) {
+        EmbedBuilder eb = context.getNormalEmbed();
+        eb.setThumbnail(member.getUser().getEffectiveAvatarUrl());
+        eb.addField(context.getTranslated("userinfo.user") + member.getUser().getName() + "#" + member.getUser().getDiscriminator(), "", false);
+        eb.addField("ID: ", member.getUser().getId() + "", false);
+        eb.addField("Status:", member.getOnlineStatus() + "", false);
+        eb.addField(context.getTranslated("userinfo.game"), member.getGame() + "", false);
+        eb.addField(context.getTranslated("userinfo.roles"), member.getRoles() + "", false);
+        context.send(eb.build());
     }
 }
