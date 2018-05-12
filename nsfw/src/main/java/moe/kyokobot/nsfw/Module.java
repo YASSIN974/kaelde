6889b8f3d6989e8c11521be92fbf7e7709d7b064
gabixdev <gabixdeveloper@gmail.com>
2018-05-12 18:31:52 +0200

    missing nsfw module files

---
+++ nsfw/src/main/java/moe/kyokobot/nsfw/Module.java
@@ -0,0 +1,45 @@
+package moe.kyokobot.nsfw;
+
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+import moe.kyokobot.bot.Settings;
+import moe.kyokobot.bot.command.Command;
+import moe.kyokobot.bot.manager.CommandManager;
+import moe.kyokobot.bot.module.KyokoModule;
+import moe.kyokobot.nsfw.commands.LewdNekoCommand;
+import net.dv8tion.jda.core.entities.Guild;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Module implements KyokoModule {
+    private Logger logger;
+    @Inject
+    private CommandManager commandManager;
+    @Inject
+    private Settings settings;
+    private ArrayList<Command> commands;
+    private HashMap<Guild, Long> cooldowns;
+    private Gson gson;
+
+    public Module() {
+        logger = LoggerFactory.getLogger(getClass());
+        commands = new ArrayList<>();
+        cooldowns = new HashMap<>();
+        gson = new Gson();
+    }
+
+    @Override
+    public void startUp() {
+        commands.add(new LewdNekoCommand(gson));
+        commands.forEach(commandManager::registerCommand);
+    }
+
+    @Override
+    public void shutDown() {
+        commands.forEach(commandManager::unregisterCommand);
+    }
+
+}
