6d9087de5dd3dd777425f54490ba699437fa2589
Sam <ophisbonklol@gmail.com>
2018-06-23 15:35:16 +0100

    gradle updates/structure changes

--- core/src/main/java/moe/kyokobot/bot/util/RandomUtil.java
+++ core/src/main/java/moe/kyokobot/bot/util/RandomUtil.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class RandomUtil {
     public static Random random = new Random();
 
+    @SafeVarargs
     public static <T> T randomElement(T... items) {
         return items[random.nextInt(items.length)];
     }
