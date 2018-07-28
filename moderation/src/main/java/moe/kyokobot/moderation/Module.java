c006a1f543930c3ec5730cb14fd9545caad68897
Sam <ophisbonklol@gmail.com>
2018-07-28 18:36:08 +0100

    add kick command + register

--- moderation/src/main/java/moe/kyokobot/moderation/Module.java
+++ moderation/src/main/java/moe/kyokobot/moderation/Module.java
@@ -6,6 +6,7 @@ import moe.kyokobot.bot.manager.CommandManager;
 import moe.kyokobot.bot.manager.DatabaseManager;
 import moe.kyokobot.bot.module.KyokoModule;
 import moe.kyokobot.bot.util.EventWaiter;
+import moe.kyokobot.moderation.commands.KickCommand;
 import moe.kyokobot.moderation.commands.SettingsCommand;
 
 import java.util.ArrayList;
@@ -30,7 +31,7 @@ public class Module implements KyokoModule {
         commands = new ArrayList<>();
 
         commands.add(new SettingsCommand(eventWaiter, databaseManager));
-
+        commands.add(new KickCommand());
         commands.forEach(commandManager::registerCommand);
     }
 
---
+++ moderation/src/main/java/moe/kyokobot/moderation/commands/KickCommand.kt
@@ -0,0 +1,65 @@
+package moe.kyokobot.moderation.commands
+
+import io.sentry.Sentry
+import moe.kyokobot.bot.command.Command
+import moe.kyokobot.bot.command.CommandCategory
+import moe.kyokobot.bot.command.CommandContext
+import moe.kyokobot.bot.command.CommandIcons
+import moe.kyokobot.bot.util.CommonErrors
+import moe.kyokobot.bot.util.UserUtil
+import net.dv8tion.jda.core.Permission
+import net.dv8tion.jda.core.entities.Member
+
+class KickCommand: Command() {
+    init {
+        name = "kick"
+        description = "moderation.kick.description"
+        usage = "moderation.usage"
+        category = CommandCategory.MODERATION
+    }
+
+    override fun execute(context: CommandContext) {
+        if (!context.selfMember.hasPermission(Permission.KICK_MEMBERS)) {
+            CommonErrors.noPermissionBot(context, Permission.KICK_MEMBERS)
+            return
+        }
+        if (!context.member.hasPermission(Permission.KICK_MEMBERS)) {
+            CommonErrors.noPermissionUser(context)
+            return
+        }
+        if (!context.hasArgs()) {
+            CommonErrors.usage(context)
+            return
+        }
+        val memberName = context.args[0]
+        val member: Member? = UserUtil.getMember(context.guild, memberName)
+        if (member == null) {
+            CommonErrors.noUserFound(context, memberName)
+            return
+        }
+        val formattedName = "${member.user.name}#${member.user.discriminator}"
+        if (!context.member.canInteract(member)) {
+            val name = String.format(context.getTranslated("moderation.kick.cannotkick"), formattedName)
+            context.send("${CommandIcons.ERROR}$name")
+            return
+        }
+        try {
+            val (reasonObj, reasonString) = if (context.args.size > 1) {
+                val arg = context.args[1]
+                Pair(arg, arg)
+            } else {
+                Pair(null, "No reason provided.")
+            }
+            context.guild.controller.kick(member, reasonObj).queue({
+                val translated = String.format(context.getTranslated("moderation.kick.output"), formattedName, reasonString)
+                context.send("${CommandIcons.SUCCESS}$translated")
+            }) { err ->
+                Sentry.capture(err)
+                context.send("${CommandIcons.ERROR}Error when kicking Guild Member $formattedName: ${err.message}")
+            }
+        } catch (err: Throwable) {
+            Sentry.capture(err)
+            context.send("${CommandIcons.ERROR}Error when kicking Guild Member $formattedName: ${err.message}")
+        }
+    }
+}
