88d416291473269e4075b1c55b1b4cabaa3d6525
Sam <ophisbonklol@gmail.com>
2018-07-30 19:11:57 +0100

    add a mod command class

---
+++ moderation/src/main/java/moe/kyokobot/moderation/ModerationCommand.kt
@@ -0,0 +1,39 @@
+package moe.kyokobot.moderation
+
+import moe.kyokobot.bot.command.Command
+import moe.kyokobot.bot.command.CommandCategory
+import moe.kyokobot.bot.command.CommandContext
+import moe.kyokobot.bot.util.CommonErrors
+import net.dv8tion.jda.core.Permission
+
+open class ModerationCommand(name: String, private vararg val permissions: Permission = emptyArray(), private val hasArgs: Boolean = true): Command() {
+    init {
+        super.name = name
+        description = "moderation.$name.description"
+        usage = "moderation.$name.usage"
+        category = CommandCategory.MODERATION
+    }
+
+    override fun preExecute(context: CommandContext) {
+        if (hasArgs && !context.hasArgs()) {
+            CommonErrors.usage(context)
+            return
+        }
+        if (context.member.hasPermission(*permissions)) {
+            CommonErrors.noPermissionUser(context)
+            return
+        }
+        /* -- An iterator is used over varargs here so I can get the specific permission the bot lacks. -- */
+        var failed = false
+        permissions.forEach { perm ->
+            if (!context.selfMember.hasPermission(perm)) {
+                CommonErrors.noPermissionBot(context, perm)
+                failed = true
+                return
+            }
+        }
+        if (failed)
+            return
+        super.preExecute(context)
+    }
+}
