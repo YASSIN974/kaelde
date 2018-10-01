c5d3c4737f56983fc00a3c51d8cb3f851cf554ec
Sam Pritchard <ophisbonklol@gmail.com>
2018-10-01 15:21:55 +0100

    join channel command + translation strings

---
+++ music/src/main/java/moe/kyokobot/music/commands/JoinCommand.kt
@@ -0,0 +1,58 @@
+package moe.kyokobot.music.commands
+
+import moe.kyokobot.bot.command.Command
+import moe.kyokobot.bot.command.CommandContext
+import moe.kyokobot.bot.command.CommandIcons
+import moe.kyokobot.bot.util.CommonErrors
+import net.dv8tion.jda.core.Permission
+
+class JoinCommand: MusicCommand() {
+    init {
+        name = "join"
+    }
+
+    override fun execute(context: CommandContext) {
+        try {
+            val state = context.member.voiceState
+            val channel = if (context.hasArgs()) {
+                context.guild.voiceChannels.firstOrNull { it.name.startsWith(context.concatArgs, true) }
+            } else {
+                state.channel
+            }
+            if (channel == null) {
+                context.send(CommandIcons.ERROR + context.getTranslated("music.join.nochannelfound"))
+                return
+            }
+            val selfState = context.selfMember.voiceState
+            if (selfState.inVoiceChannel()) {
+                if (channel.idLong == selfState.channel.idLong) {
+                    context.send(CommandIcons.ERROR + context.getTranslated("music.join.insamechannel").format(channel.name))
+                    return
+                }
+                if (context.member.canInteract(context.selfMember)) {
+                    if (context.member.hasPermission(Permission.VOICE_MOVE_OTHERS) || context.member.hasPermission(Permission.ADMINISTRATOR)) {
+                        context.guild.controller.moveVoiceMember(context.selfMember, channel).queue({
+                            context.send(CommandIcons.SUCCESS + context.getTranslated("music.join.joinedchannel").format(channel.name))
+                        }) {
+                            CommonErrors.exception(context, it)
+                        }
+                        return
+                    }
+                    val manager = context.guild.audioManager
+                    if (manager.isConnected)
+                        manager.closeAudioConnection()
+                    manager.openAudioConnection(channel)
+                    context.send(CommandIcons.SUCCESS + context.getTranslated("music.join.joinedchannel").format(channel.name))
+                    return
+                }
+                context.send(CommandIcons.ERROR + context.getTranslated("music.join.cannotinteract"))
+                return
+            }
+            context.guild.audioManager.openAudioConnection(channel)
+            context.send(CommandIcons.SUCCESS + context.getTranslated("music.join.joinedchannel").format(channel.name))
+        } catch (err: Throwable) {
+            Command.logger.error("Error when attempting to join a channel!", err)
+            CommonErrors.exception(context, err)
+        }
+    }
+}
